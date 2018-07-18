1c194ccdf3366ca9e9b6cd4b5ba15bca4216694b
Hari Dileepkumar <Hari.Dileepkumar@neudesic.com>
2018-07-19 03:33:06 +0530

    Weather App

---
+++ WeatherApplication/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public ActionResult WeatherApplication()
+        {
+            string value = "";
+            WebClient client = new WebClient();
+           value = client.DownloadString("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+            dynamic jobject = JObject.Parse(value);
+            var wind = jobject.wind.speed;
+            var location = jobject.name;
+            var weather = jobject.weather[0].description;
+
+            ViewData["WeatherLocation"] = location;
+            ViewData["Weatherweather"] = weather;
+            ViewData["Weatherwind"] = wind;
+            return View();
+        }
+    }
+}
---
+++ WeatherApplication/Models/WeatherModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Models
+{
+    public class WeatherModel
+    {
+        public String Location { get; set; }
+        public String Wind { get; set; }
+        public String Weather { get; set; }
+    }
+}
